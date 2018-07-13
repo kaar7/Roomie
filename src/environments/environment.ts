eee7abcb19a0e7d81c361bc525f2e8095a3e1c27
kaar7 <kaarthicvignesh@gmail.com>
2018-07-13 20:55:38 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDwQXzvFbI4ZKBdmZXAH69rfv6mgH_2fYY",
-    authDomain: "roomie-ea100.firebaseapp.com",
-    databaseURL: "https://roomie-ea100.firebaseio.com",
-    projectId: "roomie-ea100",
-    storageBucket: "roomie-ea100.appspot.com",
-    messagingSenderId: "1069999004613"
+   
   }
 };
 
